2b9392107ccf29f88d1ceae3a4c5c5a28e97b4f6
Urban <urbainishimwe@gmail.com>
2019-10-02 11:25:34 +0200

    - integrate app with heroku staging and production - remove unnecessary dependencies [DevOps]

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// need to be added after app.use(routers) to handle request not found
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: `${req.method}=${req.protocol}://${req.hostname}${req.path} not found`,
+  });
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   // eslint-disable-next-line no-console
@@ -15,4 +22,3 @@ const server = app.listen(process.env.PORT || 3000, () => {
 setupSwagger(app, server.address().port);
 
 export default server;
-
