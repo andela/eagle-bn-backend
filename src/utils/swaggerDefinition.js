13a2ba7510b8745bf81068cae086b75e1b866ab7
lemoissonM <murhulametre@gmail.com>
2019-10-02 08:35:09 +0200

    chore(setup-documentation) setup swagger documentation add swagger-ui-express and swagger-jsdoc packages add swagger definition add documentation routes [finishes #168781800]

---
+++ src/utils/swaggerDefinition.js
@@ -0,0 +1,34 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const setupSwagger = (app, port) => {
+  const swaggerDefinition = {
+    info: {
+      title: 'Barefoot Nomad',
+      version: '1.0.0',
+      description: 'Make company global travel and accommodation easy and convenient for the strong workforce of savvy members of staff, by leveraging the modern web.',
+    },
+    host: `localhost:${port}`,
+    basePath: '/',
+    securityDefinitions: {
+      bearerAuth: {
+        type: 'apiKey',
+        name: 'Authorization',
+        scheme: 'bearer',
+        in: 'header',
+      },
+    },
+  };
+  const options = {
+    swaggerDefinition,
+    apis: ['./src/routes/**/*.js', './src/models/**/*.js'],
+  };
+  const swaggerSpec = swaggerJsdoc(options);
+
+  app.get('/swagger.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+export default setupSwagger;
