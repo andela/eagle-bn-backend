deea8a18f654023b34c51db388a4a1188678f508
Urban <urbainishimwe@gmail.com>
2019-11-06 14:06:12 +0200

    - mail service to handle all mailing

--- src/controllers/role.js
+++ src/controllers/role.js
@@ -1,23 +1,15 @@
 import db from '../database/models/index';
 import sendResult from '../utils/sendResult';
-import { transporter } from '../config/index';
-import stringHelper from '../utils/stringHelper';
+import EmailService from '../services/email.service';
 
 const role = {
   changeRole: async (req, res) => {
     const newRole = await db.Roles.findOne({ where: { id: req.user.RoleId } });
     // check if user has the same role as the new role
     if (newRole.roleValue === req.body.new_role) return sendResult(res, 400, 'User already has this role');
-
     await db.Users.update({ RoleId: req.new_role_id }, { where: { email: req.body.email } });
 
-    await transporter.sendMail({
-      from: process.env.MAIL_SENDER,
-      to: req.body.email,
-      subject: 'no-reply barefoot nomad ✔',
-      html: stringHelper.changeRoleEmail(req)
-    });
-
+    await EmailService.roleChanged(req);
     const data = {
       status: 200,
       message: `Super admininstrator successfully changed role of ${req.body.email}`,
