4b6b85a800dc380bb4f061e6e2089fc3d8b3b893
lemoissonM <murhulametre@gmail.com>
2019-11-08 07:23:00 +0200

    chore(refactor-swagger) refator all swagger documentation [finishes #169406919]

--- src/database/models/role.js
+++ src/database/models/role.js
@@ -1,22 +1,3 @@
-/**
- * @swagger
- *
- * definitions:
- *   Role:
- *     type: object
- *     required:
- *       - roleName
- *       - roleValue
- *     properties:
- *       roleName:
- *         type: string
- *       roleValue:
- *         type: string
- *       createdAt:
- *         type: string
- *       updatedAt: 
- *         type: string
- */
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Roles = sequelize.define('Roles', {
