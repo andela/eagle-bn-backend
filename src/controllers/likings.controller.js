258b009e7a1841eca79ff8d2b0d56ddf5c8196c2
Elisha-Wigwe Chijioke O <elchroy@gmail.com>
2019-11-12 11:25:26 +0100

    Revert "#169645673 accommodation likes statistics"

--- src/controllers/likings.controller.js
+++ src/controllers/likings.controller.js
@@ -10,16 +10,6 @@ const LikingsController = {
     } catch (error) {
       return sendResult(res, 400, 'Wrong accommodation ID sent');
     }
-  },
-
-  async getAccommdationLikes(req, res) {
-    try {
-      const { accommodationId } = req.params;
-      const number = await LikingService.getLikes(accommodationId);
-      return sendResult(res, 200, ` The ccommodation ${accommodationId} has:  ${number.likes} likes `, number);
-    } catch (error) {
-      return sendResult(res, 400, 'Wrong accommodation ID sent');
-    }
   }
 };
 
